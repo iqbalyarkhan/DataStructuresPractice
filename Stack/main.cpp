ed8224440bb0b53c8bfa6e43f9021f591afd6207
Iqbal Khan <iqbalkhan@Iqbals-MacBook-Air.local>
2017-08-27 14:25:50 -0500

    Cleans up code and frees memory

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -13,28 +13,19 @@ int main() {
     s->push(3);
     s->push(2);
     s->push(1);
-<<<<<<< HEAD
     s->push(0);
-=======
     s->push(5);
->>>>>>> stack
     s->print();
     cout << "Peeking: " << s->peek() << endl;
     cout << "Pop: " << s->pop() << endl;
     cout << "Pop: " << s->pop() << endl;
     cout << "Pop: " << s->pop() << endl;
-<<<<<<< HEAD
     cout << "Pop: " << s->pop() << endl;
     cout << "Pop: " << s->pop() << endl;
     cout << "Pop: " << s->pop() << endl;
 
+    delete s;
+
     return 0;
 }
-=======
-    cout << "Peeking: " << s->peek() << endl;
-    cout << "Pop: " << s->pop() << endl;
-    cout << "Pop: " << s->pop() << endl;
-    cout << "Min is: " << s->getMin() << endl;
-    return 0;
-}
->>>>>>> stack
+   
