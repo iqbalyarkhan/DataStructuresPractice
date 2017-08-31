f814b0e8340e52dc750731a62aff79215890de6d
Iqbal Khan <iqbalkhan@Iqbals-MacBook-Air.local>
2017-08-31 18:02:20 -0500

    Adds modified Graph class \n

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,5 +1,5 @@
 //
-// Created by Iqbal Khan on 8/27/17.
+// Created by Iqbal Khan on 8/31/17.
 //
 
 #ifndef GRAPHS_GRAPH_H
@@ -7,50 +7,97 @@
 
 #include <stdio.h>
 #include <string>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 template <typename T>
 class Graph{
 
+public:
+
+    Graph(int num);
+    void initializeVertexList();
+    void printVertexInfo();
+
+
+
+
+
 private:
-    template <typename T>
-    struct AdjacencyListNode{
 
+    struct Node{
+
+        Node* next;
         T data;
-        AdjacencyListNode* next;
-
-    };
-    template <typename T>
-    struct AdjacencyList{
-
-        AdjacencyListNode* head;
+        Node* neighbors;
 
     };
 
     int numberOfVertices;
-    AdjacencyList* arrayOfVertices;
+    Node* head;
+};
+template <typename T>
+Graph<T>::Graph(int num): numberOfVertices(num), head(nullptr) {
 
-public:
-    /**
-     * Constructor that initializes the list of
-     * vertices with the number passed in by the user
-     * @param num - number of vertices in the graph
-     */
-    Graph(int num):numberOfVertices(num){
+    initializeVertexList();
 
-        arrayOfVertices = new AdjacencyList[numberOfVertices];
+}
 
-        for (int i = 0; i < numberOfVertices; ++i){
+/**
+ * Function to initialize vertex list by asking the user
+ * for input. List is ordered based on user input. Similar to
+ * add to head functionality of linked lists.
+ * @tparam T - Data inside the body is vertex information
+ */
 
-            arrayOfVertices->head= nullptr;
+template <typename T>
+void Graph<T>::initializeVertexList() {
 
-        }
+    T data;
+    cout << "Enter name or number of each vertex: " << endl;
+    cin >> data;
+    Node* temp = new Node;
+    temp->next = nullptr;
+    temp->neighbors = nullptr;
+    temp->data = data;
+    head = temp;
+    Node* curr = head;
+    cin.clear();
+    for (int i = 1; i < numberOfVertices; ++i){
 
+        cout << "Enter data for vertex: " << endl;
+        cin >> data;
+        Node* newNode = new Node;
+        newNode->data =data;
+        curr->next = newNode;
+        curr->neighbors = nullptr;
+        curr = curr->next;
 
     }
 
-};
+}
 
+/**
+ * Function to print the vertices only.
+ * @tparam T - Data held by the vertex
+ */
+
+template <typename T>
+void Graph<T>::printVertexInfo() {
+
+    Node* curr = head;
+
+    while (curr != nullptr){
+
+        cout << curr->data <<" ";
+        curr = curr->next;
+
+    }
+
+    cout << endl;
+
+}
 
 #endif //GRAPHS_GRAPH_H
